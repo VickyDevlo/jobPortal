b22845d04375a015426381d3fe2e12b0c1b8f561
vickyDevlo <vickymagare1996@gmail.com>
2024-12-20 14:40:39 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl pt-12 font-bold text-center underline">
+        Job Portal
+      </h1>
+    </div>
+  );
+}
+
+export default App;
