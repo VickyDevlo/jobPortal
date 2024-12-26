028a4b81a1ded49f643e5e2cae2c6454134c55cb
vickyDevlo <vickymagare1996@gmail.com>
2024-12-26 13:25:41 +0530

    server setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/db.js";
+import { clerkWebHooks } from "./controllers/webHooks.js";
 
 const app = express();
+
+await connectDB();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("API Working..."));
+
+app.post('/webhooks', clerkWebHooks)
 
 const PORT = process.env.PORT || 8000;
 
