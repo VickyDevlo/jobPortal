f5fe6b69fe7b316eeba47b914f6c70167d1d0c2a
vickyDevlo <vickymagare1996@gmail.com>
2025-01-10 21:56:29 +0530

    changes done

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -61,7 +61,7 @@ export const loginCompany = async (req, res) => {
   try {
     const company = await Company.findOne({ email });
 
-    if (bcrypt.compare(password, company.password)) {
+    if (await bcrypt.compare(password, company.password)) {
       return res.json({
         success: true,
         company: {
