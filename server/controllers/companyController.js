b1e6cc60e12642188689d66eca298a9a484f36e6
vickyDevlo <vickymagare1996@gmail.com>
2025-01-10 20:15:41 +0530

    changes done

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,8 +1,8 @@
 import bcrypt from "bcrypt";
 import Company from "../models/Company.js";
 import { v2 as cloudinary } from "cloudinary";
-import generateToken from "../utils/generateToken.js"; 
-import Job from '../models/Job.js'
+import generateToken from "../utils/generateToken.js";
+import Job from "../models/Job.js";
 
 export const registerCompany = async (req, res) => {
   const { name, email, password } = req.body;
@@ -178,4 +178,3 @@ export const changeVisibility = async (req, res) => {
 export const getCompanyJobApplicants = async (req, res) => {};
 
 export const changeJobApplicationsStatus = async (req, res) => {};
-
